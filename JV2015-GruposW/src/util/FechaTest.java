c7024bb9c7a7d43c05d865f76725e97bd0a7d669
Rojo <Rojo@DESKTOP-5JI8QKE.wifi.educarm.net>
2016-05-26 17:24:43 +0200

    .

---
+++ JV2015-GruposW/src/util/FechaTest.java
@@ -0,0 +1,209 @@
+package util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FechaTest {
+
+	@Test
+	public void testFechaIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFecha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFechaFecha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAño() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAño() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDifSegundos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDifMinutos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDifHoras() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDifDias() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDifSemanas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDifMeses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDifAños() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSegundos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMinutos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddHoras() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddDias() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSemanas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMeses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAños() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}

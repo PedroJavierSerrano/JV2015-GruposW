f863e6bda67b0dec216c305ba1a4c73a172dff41
Rojo <Rojo@DESKTOP-5JI8QKE.wifi.educarm.net>
2016-06-01 16:30:36 +0200

    PedroJavierSerrano

---
+++ JV2015-GruposW/src/util/test/FechaTest.java
@@ -0,0 +1,181 @@
+package util.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import util.Fecha;
+
+/** Realiza un test sobre la clase Fecha del modelo.
+ * 
+ * 
+ * @author Pedro Javier Serrano Balsalobre
+ * 31/05/2016
+ */
+
+public class FechaTest {
+	 private Fecha fecha1;
+	 private Fecha fecha2;
+	 private Fecha fecha3;
+	 
+	 private static Calendar calendario;
+	
+	 @Before
+		public void crearObjetosPrueba(){
+			 fecha1 = new Fecha();
+			 fecha2 = new Fecha("03/06/2016");
+			 fecha3 = new Fecha(fecha2);
+		}
+		
+		@After 
+		public void borrarObjetosPrueba(){
+			fecha1 = null;
+			fecha2 = null;
+			fecha3 = null;
+			}
+	 
+	
+	 
+	@Test
+	public void testFechaIntIntInt() {
+		assertTrue(fecha2 != null);
+	}
+
+	@Test
+	public void testFecha() {
+		assertTrue(fecha2 != null);
+	}
+
+	@Test
+	public void testFechaFecha() {
+		assertTrue(fecha1 != null);
+	}
+
+	@Test
+	public void testGetAño() {
+		assertTrue(fecha3 != null);
+	}
+
+	@Test
+	public void testGetMes() {
+		assertTrue(fecha2 != null);
+	}
+
+	@Test
+	public void testGetDia() {
+		assertTrue(fecha2 != null);
+	}
+
+	@Test
+	public void testSetAño() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public long testDifSegundos() {
+		return ((calendario.getTimeInMillis() 
+				- FechaTest.calendario.getTimeInMillis()) / 1000);
+	}
+
+	@Test
+	public long testDifMinutos() {
+		return (long)((calendario.getTimeInMillis() 
+				- FechaTest.calendario.getTimeInMillis()) /60*1000);
+	}
+
+	@Test
+	public long testDifHoras() {
+		return (long) (calendario.getTimeInMillis() 
+				- FechaTest.calendario.getTimeInMillis()) / (60*60*1000);
+	}
+
+	@Test
+	public int testDifDias() {
+		return (int)(calendario.getTimeInMillis() 
+				- FechaTest.calendario.getTimeInMillis()) / (24*60*60*1000);
+	}
+
+	@Test
+	public int testDifSemanas() {
+		return (int) (calendario.getTimeInMillis() 
+				- FechaTest.calendario.getTimeInMillis()) / (7*60*60*1000);
+	}
+
+	@Test
+	public int testDifMeses() {
+		return (int) (calendario.getTimeInMillis() 
+				- FechaTest.calendario.getTimeInMillis()) / (30*60*60*1000);
+	}
+
+	@Test
+	public int testDifAños() {
+		return (int) (calendario.getTimeInMillis() 
+				- FechaTest.calendario.getTimeInMillis()) / (365*60*60*1000);
+	}
+
+	@Test
+	public void testAddSegundos() {
+		int segundos = 10;
+		calendario.add(Calendar.SECOND, segundos);
+	}
+
+	@Test
+	public void testAddMinutos() {
+		int minutos = 5;
+		calendario.add(Calendar.MINUTE, minutos);
+	}
+
+	@Test
+	public void testAddHoras() {
+		int horas = 1;
+		calendario.add(Calendar.HOUR, horas);
+	}
+
+	@Test
+	public void testAddDias() {
+		int dias = 1;
+		calendario.add(Calendar.DAY_OF_MONTH, dias);
+	}
+
+	@Test
+	public void testAddSemanas() {
+		int semanas = 2;
+		calendario.add(Calendar.WEEK_OF_MONTH, semanas);
+	}
+
+	@Test
+	public void testAddMeses() {
+		int meses = 1;
+		calendario.add(Calendar.MONTH,meses);
+	}
+
+	@Test
+	public void testAddAños() {
+		int años = 1;
+		calendario.add(Calendar.YEAR,años);
+	}
+
+	
+
+	@Test
+	public int testCompareTo() {
+		
+		return calendario.compareTo(FechaTest.calendario);
+	}
+	
+}
+	
+
